cd117f9919bfae9cf3d048edaaa6ee64b0f666a1
Aditya Gupta <ag15035@gmail.com>
2022-06-02 11:49:52 +0530

    routes: /student/submit-for-approval: Implement

--- src/api/student/controllers/student.js
+++ src/api/student/controllers/student.js
@@ -7,6 +7,7 @@
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::student.student", ({ strapi }) => ({
+  /* This route is accessible to unauthenticated public also */
   async register(ctx) {
     /* Request body is expected to be exactly same as if it was a POST request to create entry through strapi REST api
      * ie. ctx.request.body should be like: { data:{"name":"Koi","roll": "1905050"} }
@@ -18,7 +19,48 @@ module.exports = createCoreController("api::student.student", ({ strapi }) => ({
 
     // Ensure, sender did not sender with "approved: true", since the field is eitherway visible to the student
     data["approved"] = "created";
+
+    // Pass result as it is, shouldn't contain anything sensitive
     ctx.body = await strapi.db.query("api::student.student").create({data});
+  },
+
+  /* Accessible only with proper bearer token 
+     Idea is to first, create a request, and then set "approved": "pending",
+  */
+  async submit_for_approval(ctx) {
+    const user = ctx.state.user;
+
+    if (!user) {
+      return ctx.badRequest(null, [{ messages: [{ id: "No user found" }] }]);
+    }
+
+    const data = await strapi.db.query("api::student.student").update({
+      where: {
+        roll: user.username,
+      },
+      data: {
+        approved: "pending"
+      },
+      select: ["roll","approved","id"]
+    });
+
+    if (!data) {
+      return ctx.internalServerError(null, [{ messages: [{ id: "Failed to update student's approval status" }] }]);
+    }
+
+    const request = await strapi.db.query("api::request.request").create({
+      data: {
+        description: "Request for student approval",
+        type: "student",
+        student: data.id
+      }
+    });
+
+    if (!request) {
+      return ctx.internalServerError(null, [{ messages: [{ id: "Failed to create student approval request" }] }]);
+    }
+
+    ctx.body = request;
   },
 
 
--- src/api/student/routes/custom-student.js
+++ src/api/student/routes/custom-student.js
@@ -18,5 +18,11 @@ module.exports = {
       handler: "student.register",
       policies: []
     },
+    {
+      method: "POST",
+      path: "/student/submit-for-approval",
+      handler: "student.submit_for_approval",
+      policies: []
+    }
   ],
 };
