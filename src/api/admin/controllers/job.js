423084d13beed7312b1162343e2e3ae6446e14e7
Aditya Gupta <ag15035@gmail.com>
2022-06-28 00:43:25 +0530

    admin: job: Use helper function
    Using the helper functions ensures all conditions are at one place

--- src/api/admin/controllers/job.js
+++ src/api/admin/controllers/job.js
@@ -1,4 +1,4 @@
-const { helper_get_applications } = require("../../student/controllers/job");
+const { helper_is_job_eligible, helper_get_applications } = require("../../student/controllers/util");
 
 module.exports = {
     /**
@@ -9,6 +9,7 @@ module.exports = {
      * @queryParam {String} roll
      * 
      * @note This is a duplicate API of student.get_eligible_jobs, this is for admin, to get eligible jobs for a given roll number
+     * @note If you want to know more about the logic, see student.get_eligible_jobs
      *
      * @example http://localhost:3000/api/admin/job/get_eligible_jobs?roll=1
      * 
@@ -23,15 +24,14 @@ module.exports = {
 
         const student_self = await strapi.db.query("api::student.student").findOne({
             where: {
-                roll: roll
+                roll: roll,
             },
-            select: ["id", "approved", "X_marks", "XII_marks", "cpi", "program", "department", "registered_for"]
         });
         if (!student_self) {
-            return ctx.notFound(null, [{ messages: [{ id: 'Student not found' }] }]);
+            return ctx.notFound(null, [{ messages: [{ id: "Student not found" }] }]);
         }
 
-        const { id, approved, X_marks, XII_marks, cpi, program, department, registered_for } = student_self;
+        const { id, approved, X_marks, XII_marks, cpi, registered_for } = student_self;
 
         if (approved !== "approved") {
             return ctx.badRequest(null, [{ messages: [{ id: "Account not approved yet" }] }]);
@@ -43,54 +43,22 @@ module.exports = {
 
         let eligible_jobs = await strapi.db.query("api::job.job").findMany({
             where: {
-                min_X_marks: {
-                    $lte: X_marks
-                },
-                min_XII_marks: {
-                    $lte: XII_marks
-                },
-                min_cpi: {
-                    $lte: cpi
-                },
-                // NOTE: eligible_departments is handled after this query
-                // NOTE2: eligible_program is mandatory field in Job collection, and must contain name of only 1 program, eg. B.Tech
-                eligible_program: program,
-
-                // Only jobs that are approved will be shown to student
+                min_X_marks: { $lte: X_marks },
+                min_XII_marks: { $lte: XII_marks },
+                min_cpi: { $lte: cpi },
                 approval_status: "approved",
-                // TODO: Find ways to query this, not working at all for now
-                // For now doing comparison with last date later in this function
-                // last_date: {
-                //     $gte: (new Date)
-                // },
-
-                // Filter based on if user registered for "Internship" or "FTE"
+                job_status: "open",
                 category: registered_for,
             },
             populate: ["company", "jaf"]
         });
-        // console.log(eligible_jobs);
 
-        // console.log({ id, approved, X_marks, XII_marks, registered_for, date: Date.now(), is_in_future: "2022-06-08T18:49:23.001Z" < Date.now(), eligible_jobs });
-
-        if (!eligible_jobs || !Array.isArray(eligible_jobs)) {
+        if (!Array.isArray(eligible_jobs)) {
             return ctx.internalServerError(null, [{ messages: [{ id: "Could not get eligible jobs" }] }]);
         }
 
-        // Assumption: job.eligible_departments is a string of comma separated CASE-INSENSITIVE department names, eg. "mathematics,computer science"
-        eligible_jobs = eligible_jobs.filter(job => {
-            // Filter based on job.eligible_departments if it's not empty
-            if (job.eligible_departments) {
-                // Case insensitive
-                let lowercase_dep = department.toLowerCase();
-                return job.eligible_departments.split(",").map(dep => dep.toLowerCase()).includes(lowercase_dep);
-            } else {
-                return true;
-            }
-        });
-
         // Check applications in which student has been selected
-        const selected_jobs = await strapi.db.query("api::application.application").findMany({
+        const selected_applications = await strapi.db.query("api::application.application").findMany({
             where: {
                 student: id,
                 status: "selected"
@@ -98,87 +66,16 @@ module.exports = {
             populate: ["job"]
         });
 
-        const number_of_A1_applications = await strapi.db.query("api::application.application").count({
-            where: {
-                student: id,
-                // TODO: Add logic to not count applications that are in "rejected" status, IF THIS IS REQUIRED BY SPECS
-            }
-        });
-
         /**
-         * `exists` is an array of bools, representing whether a job has been already applied for
+         * `Array.filter` doesn't support async function write now, so using this 'trick'
          * 
          * @ref: https://advancedweb.hu/how-to-use-async-functions-with-array-filter-in-javascript/
          */
-        const exists = await Promise.all(eligible_jobs.map(async (job) => {
-            try {
-                // Check if current datetime is more than job's last datetime (ie. apply date passed)
-                if (Date.now() > Date.parse(job.last_date)) {
-                    console.debug(`Roll: ${roll}, Ineligible, Reason: Date passed`);
-                    return false;
-                }
-            } catch (err) {
-                console.debug(`[job: get_eligible_jobs]: Job: ${job.job_title} may have invalid last date: ${job.last_date}`, { err });
-            }
+        const is_eligible = await Promise.all(eligible_jobs.map(
+            async (job) => (await helper_is_job_eligible(student_self, job, selected_applications))
+        ));
 
-            // Ensure that these conditions are met:
-            // 1. If job.classification is "X", then the 'below conditions' will be null and void, except required qualifications which are already checked, so return true
-            // 2. If selected in A1, out of placement, not eligible in future
-            // 3. If selected in B1, then 3 more A1 applications allowed, AFTER selected in B1
-            // 4. If student receives 2 offers, not eligible for more applications
-            // TODO: Some of these conditions can be moved out of this loop
-            // Ensure condition 1 above
-            if (job.classification === "X") {
-                return true;
-            }
-
-            // Ensure condition 2 above
-            if (selected_jobs.find(selected_job => selected_job.job.classification === "A1") !== undefined) {
-                // Student has selected in A1
-                console.debug(`Roll: ${roll}, Ineligible, Reason: Selected in A1`);
-                return false;
-            }
-
-            // Ensure condition 3 above... checking for 3 A1 applications part to be done at apply function
-            if (selected_jobs.find(selected_job => selected_job.job.classification === "B1") != undefined) {
-                // If selected in B1 already, then other B1 jobs not eligible now
-                if (job.classification === "B1") {
-                    console.debug(`Roll: ${roll}, Ineligible, Reason: Selected in B1`);
-                    return false;
-                }
-
-                if (number_of_A1_applications >= 3) {
-                    console.debug(`Roll: ${roll}, Ineligible, Reason: Already applied for 3 A1 jobs`);
-                    return false;
-                }
-            }
-
-            // Ensures condition 4 above
-            if (selected_jobs.length >= 2) {
-                // Not eligible in any jobs
-                console.debug(`Roll: ${roll}, Ineligible, Reason: Already selected for 2 jobs`);
-                return false;
-            }
-
-            const existing_application = await strapi.db.query("api::application.application").findOne({
-                where: {
-                    student: id,
-                    job: job.id,
-                },
-                // populate: true   (Not required, just checking if it exists)
-            });
-
-            // console.log("For", {job: job.job_title, existing_application});
-            if (!existing_application) {
-                // Not yet applied, so eligible
-                return true;
-            } else {
-                console.debug(`Roll: ${roll}, Ineligible, Reason: Already applied for job`);
-                return false;
-            }
-        }));
-
-        eligible_jobs = eligible_jobs.filter((_, index) => exists[index]);
+        eligible_jobs = eligible_jobs.filter((_, index) => is_eligible[index]);
 
         ctx.body = eligible_jobs;
     },
@@ -203,7 +100,7 @@ module.exports = {
             return ctx.badRequest("Missing roll number", { received_roll: roll || null });
         }
 
-        const applied_jobs = helper_get_applications(roll);
+        const applied_jobs = await helper_get_applications(roll);
 
         ctx.body = applied_jobs;
     },
