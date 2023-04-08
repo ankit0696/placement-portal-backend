2945a8491414c97115cae7642626ade0e2f48c6f
Ankit Kumar <ankitkr0696@gmail.com>
2023-04-08 09:32:38 +0530

    update email config

--- config/plugins.js
+++ config/plugins.js
@@ -2,22 +2,22 @@ module.exports = ({ env }) => ({
   // ...
   email: {
     config: {
-      provider: "nodemailer",
+      provider: 'nodemailer',
       providerOptions: {
-        host: env("SMTP_HOST", "smtp.example.com"),
-        port: env("SMTP_PORT", 587),
-        secure: env("SMTP_SECURE", false),
-        ignoreTLS: true,
-        auth: false,
-
-        // ... any custom nodemailer options
+        host: env('SMTP_HOST', 'smtp.office365.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+          // ... any custom nodemailer options
+        },
       },
       settings: {
-        defaultFrom: "no-reply@iitp.ac.in",
-        defaultReplyTo: "no-reply@iitp.ac.in",
+        defaultFrom: 'no-reply@iitp.ac.in',
+        defaultReplyTo: 'no-reply@iitp.ac.in',
       },
     },
   },
 
   // ...
-});
+})
